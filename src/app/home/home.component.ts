90bee833f26da073efa8be505107b80a53079fec
Arafet <arafet.azzabi@esprit.tn>
2020-11-12 01:21:21 +0100

    HomeComponent

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'home',
+  templateUrl: './home.component.html'
+})
+
+export class HomeComponent  {
+
+}
